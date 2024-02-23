c96ab08d0ec2bc1c1f36ab3fe616827b75ac524a
Gui-Corby <guilhermecorbym@gmail.com>
2024-02-23 16:54:39 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -10,3 +10,80 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+
+function renderElements (tasks) {
+  let ul = document.querySelector(".tasks__list");
+
+  let taskToDelete = document.querySelectorAll(".task__item");
+
+  for (let i = 0; i < taskToDelete.length; i++) {
+    taskToDelete[i].remove();
+  }
+
+  for (let i = 0; i < tasks.length; i++) {
+    let task = createTaskItem(tasks[i]);
+    ul.appendChild(task);
+  }
+
+  return ul
+}
+
+function createTaskItem (obj) {
+  let li = document.createElement("li");
+  let div = document.createElement("div");
+  let span = document.createElement("span");
+  let p = document.createElement("p");
+  let button = document.createElement("button");
+
+  li.className = "task__item";
+  div.className = "task-info__container";
+  p.textContent = obj.title;
+  button.className = "task__button--remove-task";
+
+  if (obj.type.toLowerCase() === "urgente") {
+    span.className = "task-type span-urgent";
+  } else if (obj.type.toLowerCase() === "importante") {
+    span.className = "task-type span-important"  
+  } else {
+    span.className = "task-type span-normal"
+  }
+
+  div.appendChild(span);
+  div.appendChild(p);
+  li.appendChild(div);
+  li.appendChild(button);
+
+  button.addEventListener('click', function(event) {
+  
+    let indexToDelete = tasks.indexOf(obj);
+    if (indexToDelete !== -1) {
+      tasks.splice(indexToDelete, 1);
+    }
+    
+    renderElements (tasks);
+  })
+
+  return li;
+}
+
+let addTaskButton = document.querySelector(".form__button--add-task");
+
+  addTaskButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    let title = document.getElementById("input_title").value;
+    let prioritySelect = document.querySelector(".form__input--priority");
+    let type = prioritySelect.value;
+
+    let newObj = {
+      title: title,
+      type: type,
+    }
+    
+    tasks.push(newObj);
+    renderElements (tasks);
+      }
+    ) 
+  
+    
+ renderElements (tasks);
